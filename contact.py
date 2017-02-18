73d8c3ea1a9c84a1d2d11a6cda8cc1e36dc4b8ea
rmolewska <keirekgra@gmail.com>
2017-02-18 17:52:31 +0100

    Contact Test homework

---
+++ contact.py
@@ -0,0 +1,29 @@
+class Contact:
+    def __init__(self, first_name, middle_name, last_name, nickname, company_title, company_name,
+                 company_address, home_phone, mobile_phone, work_phone, fax, email, email_2, email_3,
+                 homepage, birth_day, birth_month, birth_year, ann_day, ann_month, ann_year, address_2,
+                 phone_2, notes):
+        self.first_name = first_name
+        self.middle_name = middle_name
+        self.last_name = last_name
+        self.nickname = nickname
+        self.company_title = company_title
+        self.company_name = company_name
+        self.company_address = company_address
+        self.home_phone = home_phone
+        self.mobile_phone = mobile_phone
+        self.work_phone = work_phone
+        self.fax = fax
+        self.email = email
+        self.email_2 = email_2
+        self.email_3 = email_3
+        self.homepage = homepage
+        self.birth_day = birth_day
+        self.birth_month = birth_month
+        self.birth_year = birth_year
+        self.ann_day = ann_day
+        self.ann_month = ann_month
+        self.ann_year = ann_year
+        self.address_2 = address_2
+        self.phone_2 = phone_2
+        self.notes = notes
