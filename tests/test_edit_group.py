a94913d8be5c16eefbe7e8a45b77c1da382b19b6
rmolewska <keirekgra@gmail.com>
2017-02-24 15:55:04 +0100

    zmiany w testach modyfikacji grup (konstrukcja if/else

--- tests/test_edit_group.py
+++
@@ -1,8 +0,0 @@
-
-from models.group import Group
-
-
-def test_edit_first_group(app):
-    app.session.login(username="admin", password="secret")
-    app.group.edit_first_group(Group(name="yyyy", header="ppppppp", footer="kkkkkkk"))
-    app.session.logout()
