cefd2b17bae949b041586f44d4c7ade3bf80d9a1
rmolewska <keirekgra@gmail.com>
2017-03-01 18:37:50 +0100

    zmiany w testach modyfikacji kontaktu (konstrukcja if/else)

--- models/contact.py
+++ models/contact.py
@@ -1,8 +1,9 @@
 class Contact:
-    def __init__(self, first_name, middle_name, last_name, nickname, company_title, company_name,
-                 company_address, home_phone, mobile_phone, work_phone, fax, email, email_2, email_3,
-                 homepage, birth_day, birth_month, birth_year, ann_day, ann_month, ann_year, address_2,
-                 phone_2, notes):
+
+    def __init__(self, first_name=None, middle_name=None, last_name=None, nickname=None, company_title=None,
+                 company_name=None, company_address=None, home_phone=None, mobile_phone=None, work_phone=None, fax=None,
+                 email=None, email_2=None, email_3=None, homepage=None, birth_year=None, ann_year=None, address_2=None,
+                 phone_2=None, notes=None):
         self.first_name = first_name
         self.middle_name = middle_name
         self.last_name = last_name
@@ -18,11 +19,7 @@ class Contact:
         self.email_2 = email_2
         self.email_3 = email_3
         self.homepage = homepage
-        self.birth_day = birth_day
-        self.birth_month = birth_month
         self.birth_year = birth_year
-        self.ann_day = ann_day
-        self.ann_month = ann_month
         self.ann_year = ann_year
         self.address_2 = address_2
         self.phone_2 = phone_2
--- tests/test_add_contact.py
+++ tests/test_add_contact.py
@@ -9,12 +9,8 @@ def test_add_contact(app):
                                    company_title="dfdsbf", company_name="dfdsf", company_address="dfdsfds",
                                    home_phone="3232132131", mobile_phone="12313213232", work_phone="2132132133",
                                    fax="213123213213", email="hshs@qwe.com", email_2="ewqe@qwe.com",
-                                   email_3="dqweweq@qwe.com", homepage="www.ff.com",
-                                   birth_day="//div[@id='content']/form/select[1]//option[31]",
-                                   birth_month="//div[@id='content']/form/select[2]//option[12]", birth_year="1986",
-                                   ann_day="//div[@id='content']/form/select[3]//option[31]",
-                                   ann_month="//div[@id='content']/form/select[4]//option[11]", ann_year="1996",
-                                   address_2="fgrfgfg", phone_2="453543543543", notes="fgdgfdgd"))
+                                   email_3="dqweweq@qwe.com", homepage="www.ff.com", birth_year="1986",
+                                   ann_year="1996", address_2="fgrfgfg", phone_2="453543543543", notes="fgdgfdgd"))
     app.session.logout()
 
 
@@ -22,11 +18,7 @@ def test_add_empty_contact(app):
     app.session.login(username="admin", password="secret")
     app.contact.create_new(Contact(first_name="", middle_name="", last_name="", nickname="", company_title="",
                                    company_name="", company_address="", home_phone="", mobile_phone="", work_phone="",
-                                   fax="", email="", email_2="", email_3="", homepage="",
-                                   birth_day="//div[@id='content']/form/select[1]//option[1]",
-                                   birth_month="//div[@id='content']/form/select[2]//option[1]", birth_year="",
-                                   ann_day="//div[@id='content']/form/select[3]//option[1]",
-                                   ann_month="//div[@id='content']/form/select[4]//option[1]", ann_year="",
+                                   fax="", email="", email_2="", email_3="", homepage="",  birth_year="", ann_year="",
                                    address_2="", phone_2="", notes=""))
     app.session.logout()
 
