c725a25533fc86d5525f0f6043ab1a60b2bb197e
rmolewska <keirekgra@gmail.com>
2017-03-02 23:10:38 +0100

    warunki wstępne dla usuwania/modyfikacji grup i kontaktow

--- tests/test_del_group.py
+++ tests/test_del_group.py
@@ -1,3 +1,8 @@
 
+from models.group import Group
+
+
 def test_delete_first_group(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="Test", header="dssdgsg", footer="sdgsdgsG"))
     app.group.delete_first_group()
--- tests/test_modify_contact.py
+++ tests/test_modify_contact.py
@@ -3,8 +3,22 @@ from models.contact import Contact
 
 
 def test_modify_contact_first_name(app):
+    if app.contact.count() == 0:
+        app.contact.create_new(Contact(first_name="Test", middle_name="Test2", last_name="Testowy", nickname="dsksljd",
+                                       company_title="dfdsbf", company_name="dfdsf", company_address="dfdsfds",
+                                       home_phone="3232132131", mobile_phone="12313213232", work_phone="2132132133",
+                                       fax="213123213213", email="hshs@qwe.com", email_2="ewqe@qwe.com",
+                                       email_3="dqweweq@qwe.com", homepage="www.ff.com", birth_year="1986",
+                                       ann_year="1996", address_2="fgrfgfg", phone_2="453543543543", notes="fgdgfdgd"))
     app.contact.modify_first_contact(Contact(first_name="New first name"))
 
 
 def test_modify_contact_middle_name(app):
+    if app.contact.count() == 0:
+        app.contact.create_new(Contact(first_name="Test", middle_name="Test2", last_name="Testowy", nickname="dsksljd",
+                                       company_title="dfdsbf", company_name="dfdsf", company_address="dfdsfds",
+                                       home_phone="3232132131", mobile_phone="12313213232", work_phone="2132132133",
+                                       fax="213123213213", email="hshs@qwe.com", email_2="ewqe@qwe.com",
+                                       email_3="dqweweq@qwe.com", homepage="www.ff.com", birth_year="1986",
+                                       ann_year="1996", address_2="fgrfgfg", phone_2="453543543543", notes="fgdgfdgd"))
     app.contact.modify_first_contact(Contact(middle_name="New middle name"))
