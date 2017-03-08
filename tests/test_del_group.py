60a813d3a79af33fcc347005b8cbdff24e0c2b64
rmolewska <keirekgra@gmail.com>
2017-03-08 22:09:06 +0100

    ładowanie listy grup

--- tests/test_del_group.py
+++ tests/test_del_group.py
@@ -3,6 +3,10 @@ from models.group import Group
 
 
 def test_delete_first_group(app):
+
     if app.group.count() == 0:
         app.group.create(Group(name="Test", header="dssdgsg", footer="sdgsdgsG"))
+    old_groups = app.group.get_group_list()
     app.group.delete_first_group()
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) - 1 == len(new_groups)
