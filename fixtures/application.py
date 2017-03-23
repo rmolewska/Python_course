23fc2b4fc88c7c400af8d051b33021d31639d849
rmolewska <keirekgra@gmail.com>
2017-03-23 21:00:29 +0100

    Zadanie 16

--- fixtures/application.py
+++ fixtures/application.py
@@ -1,4 +1,4 @@
-from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium import webdriver
 from fixtures.session import SessionHelper
 from fixtures.group import GroupHelper
 from fixtures.contact import ContactHelper
@@ -6,11 +6,19 @@ from fixtures.contact import ContactHelper
 
 class Application:
 
-    def __init__(self):
-        self.wd = WebDriver()
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "edge":
+            self.wd = webdriver.Edge()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -21,7 +29,7 @@ class Application:
 
     def open_home_page(self):
         wd = self.wd
-        wd.get("http://localhost:8080/addressbook/")
+        wd.get(self.base_url)
 
     def destroy(self):
         self.wd.quit()
