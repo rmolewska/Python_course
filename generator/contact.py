62ed54e58526392de3c833a657c675e20262a150
rmolewska <keirekgra@gmail.com>
2017-04-01 16:40:42 +0200

    Ex 18/19 - male poprawki

--- generator/contact.py
+++ generator/contact.py
@@ -59,6 +59,6 @@ file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 # with open(file, "w") as out:
 #    out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
 
-with open(file, "w") as out:
+with open(file, "w", encoding='utf8') as out:
     jsonpickle.set_encoder_options("json", indent=2)
     out.write(jsonpickle.encode(test_data))
--- generator/group.py
+++ generator/group.py
@@ -37,6 +37,6 @@ test_data = [Group(name="", header="", footer="")] + [
 
 file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
-with open(file, "w") as out:
+with open(file, "w", encoding="utf8") as out:
     jsonpickle.set_encoder_options("json", indent=2)
     out.write(jsonpickle.encode(test_data))
