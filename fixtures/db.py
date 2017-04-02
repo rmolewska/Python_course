0a05d5f71b721da439a9e2d0b2daa73ed225bfce
rmolewska <keirekgra@gmail.com>
2017-04-02 16:52:18 +0200

    Ładowanie obiektów z bazy danych dla kontaktów

--- fixtures/db.py
+++ fixtures/db.py
@@ -1,6 +1,6 @@
 import mysql.connector
 from models.group import Group
-# from models.contact import Contact
+from models.contact import Contact
 
 class DbFixture:
 
@@ -12,16 +12,31 @@ class DbFixture:
         self.connection = mysql.connector.connect(host=host, database=name, user=user, password=password)
 
     def get_group_list(self):
-        list = []
+        group_list = []
         cursor = self.connection.cursor()
         try:
             cursor.execute("select group_id, group_name, group_header, group_footer from group_list")
             for row in cursor:
                 (id, name, header, footer) = row
-                list.append(Group(id=str(id), name=name, header=header, footer=footer))
+                group_list.append(Group(id=str(id), name=name, header=header, footer=footer))
         finally:
             cursor.close()
-        return list
+        return group_list
+
+    def get_contact_list(self):
+        contact_list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select id, firstname, lastname, address, home, mobile, work, email, email2, email3 "
+                           "from addressbook where deprecated = '0000-00-00 00:00:00'")
+            for row in cursor:
+                (id, firstname, lastname, address, home, mobile, work, email, email2, email3) = row
+                contact_list.append(Contact(id=str(id),first_name=firstname, last_name=lastname, address=address,
+                                            home_phone=home, mobile_phone=mobile, work_phone=work, email=email,
+                                            email_2=email2, email_3=email3))
+        finally:
+            cursor.close()
+        return contact_list
 
     def destroy(self):
         self.connection.close()
--- tests/test_db_matches_ui.py
+++ tests/test_db_matches_ui.py
@@ -1,5 +1,6 @@
 
 from models.group import Group
+from models.contact import Contact
 
 
 def test_group_list(app, db):
@@ -25,3 +26,14 @@ def test_group_list(app, db):
 #   assert sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max)
 
 
+def test_contact_list(app, db):
+    ui_contact_list = app.contact.get_contact_list()
+
+    def clean(contact):
+        return Contact(last_name="".join(contact.last_name.strip().split()), first_name="".join(contact.first_name.strip().split()),
+                       address=contact.address, email=contact.email, id=contact.id)
+
+    db_contact_list = map(clean, db.get_contact_list())
+    ui_contact_list = map(clean, ui_contact_list)
+
+    assert sorted(ui_contact_list, key=Contact.id_or_max) == sorted(db_contact_list, key=Contact.id_or_max)
